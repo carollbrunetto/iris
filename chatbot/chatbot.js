f6ff1b568992359a2a4d5cc30324d2aabd7b525d
Caroline Brunetto de Jesus <06153726194@seguranca.local>
2022-09-30 13:23:00 -0400

    continuando configurações com API

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -1,8 +1,16 @@
 'use strict'
 const dialogflow = require('dialogflow');
-const config = require('../config/keys')
+const structjson = require('./structjson');
+const config = require('../config/keys');
+const {struct} = require('pb-util')
 
-const sessionClient = new dialogflow.SessionsClient();
+const projectId = config.googleProjectId;
+const credentials = {
+  client_email: config.googleClientEmail,
+  private_key: config.googlePrivateKey
+}
+
+const sessionClient = new dialogflow.SessionsClient({projectId: projectId, credentials: credentials});
 
 const sessionPath = sessionClient.sessionPath(config.googleProjectId, config.dialogFlowSessionId);
 
@@ -28,6 +36,22 @@ module.exports = {
     return responses;
   },
 
+  eventQuery: async function(event, parameters = {}) {
+    let self = module.exports;
+    const request = {
+      session: sessionPath,
+      queryInput: {
+          event: {
+              name: event,
+              parameters: struct.encode(parameters),  
+              languageCode: config.dialogFlowSessionLanguageCode
+          },
+      },
+    };
+    let responses = await sessionClient.detectIntent(request);
+    responses = await self.handleAction(responses);
+    return responses;
+  },
   handleAction: function(responses){
     return responses;
   }
