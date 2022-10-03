8ac0651172cdd32c042ef245409b7973ef31e915
Caroline Brunetto de Jesus <06153726194@seguranca.local>
2022-10-03 08:41:03 -0400

    conectando o server side e dialogflow

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleProjectId: process.env.GOOGLE_PROJECT_ID,
+  dialogFlowSessionId: process.env.DIALOGFLOW_SESSION_ID,
+  dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
+  googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
+  googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL
+}
