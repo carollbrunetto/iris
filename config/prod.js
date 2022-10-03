229a49eafd069ab84bf81d717690b5f815fc8934
Caroline Brunetto de Jesus <06153726194@seguranca.local>
2022-10-03 08:53:35 -0400

    tentando arrumar erro heroku

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
   googleProjectId: process.env.GOOGLE_PROJECT_ID,
   dialogFlowSessionId: process.env.DIALOGFLOW_SESSION_ID,
   dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
-  googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
+  googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
   googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL
 }
