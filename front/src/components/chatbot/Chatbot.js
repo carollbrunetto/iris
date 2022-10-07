27fcb96dd501b382834b8c5cddf3449585f02b69
Caroline Brunetto de Jesus <06153726194@seguranca.local>
2022-10-07 13:26:59 -0400

    continuando o desenvolvimento do front

--- front/src/components/chatbot/Chatbot.js
+++ front/src/components/chatbot/Chatbot.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios/index';
 
 const Chatbot = () => {
+
+  const [messages, setMessages] = useState[[]]
+
+  const df_text_query = async (text) => {
+    let says = {
+      speaks: 'me',
+      msg: {
+        text: {
+          text: text
+        }
+      }
+    }
+
+    setMessages({messages: [...messages, says]})
+
+    const res = await axios.post('/api/df_text_query', {text})
+
+    for (let msg of res.data.fulfillmentMessages) {
+      says = {
+        speaks: 'Íris',
+        msg:msg
+      }
+      setMessages({messages: [...messages, says]});
+    }
+
+  }
+
+  const df_event_query = async (text) => {
+    const res = await axios.post('/api/df_evento_query', {evento});
+
+    for (let msg of res.data.fulfillmentMessages) {
+      let says = {
+        speaks: 'me',
+        msg: msg
+      }
+      setMessages({messages: [...messages, says]})
+    } 
+  }
+
+
   return (
-    <h2>
-      Chatbot estará aqui!
-    </h2>
+    <div style={{height: 400, width: 400, float: 'right'}}>
+      <div id='chatbot' style={{height: '100%', width: '100%', overflow: 'auto'}}>
+          <h2>Chatbot</h2>
+          <input type="text"/>
+      </div>
+    </div>
   )
 };
 
