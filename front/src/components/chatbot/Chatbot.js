ac3544b35094758592633dd18b83400054a1893a
Caroline Brunetto de Jesus <06153726194@seguranca.local>
2022-11-03 13:37:18 -0400

    quick replies

--- front/src/components/chatbot/Chatbot.js
+++ front/src/components/chatbot/Chatbot.js
@@ -8,6 +8,7 @@ import {v4 as uuid} from 'uuid';
 import './Chatbot.modules.css'
 import Feedback from './Feedback';
 import Header from './Header';
+import QuickReplies from './QuickReplies';
 
 const cookies = new Cookies();
 
@@ -17,7 +18,6 @@ const Chatbot = () => {
   const [messages, setMessages] = useState([])
   const [objMessage, setObjMessage] = useState([])
   const [show, setShow] = useState(true)
-  const [fbOn, setFbOn] = useState(true)
   const [feedback, setFeedback] = useState(false)
   let userId;
 
@@ -64,27 +64,52 @@ const Chatbot = () => {
     setMessages({messages:  objMessage})
   }
 
-  const scrollToBottom = () => {
-    messagesEnd.current.scrollIntoView({ behavior: "smooth" })
+  const autoscroll = () => {
+    const newMessage = messages.lastElementChild;
+
   }
+  // const scrollToBottom = () => {
+   
+  // }
 
   useEffect  ( () => {
     // df_event_query('Welcome')
     // df_event_query('Teste')
-    scrollToBottom()
+    messagesEnd.current?.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest"})
   }, [messages])
 
+
+  const messagesQuickReplies = ((stateMessages, i) => {
+    if (stateMessages[1]?.msg && stateMessages[1]?.msg?.text && stateMessages[1]?.msg?.text?.text) {
+      console.log('entrou')
+      return <Message key={i} speaks={stateMessages[1]?.speaks} text={stateMessages[1]?.msg?.text?.text} />
+    } else if (messages.msg &&
+      messages.msg.payload &&
+      messages.msg.payload.fields &&
+      messages.msg.payload.fields.quick_replies
+      ) {
+        console.log('entrou aqui')
+        return <QuickReplies
+          text={messages.msg.payload.fields.text ? messages.msg.payload.fields.text : null}
+          key={i}
+          replyClick={handleQuickReplyPayload}
+          speaks={messages.speaks}
+          payload={messages.msg.payload.fields.quick_replies.listValue.values}/>
+      }
+  })
 
   function handleQuickReplyPayload(e, payload, text) {
     df_text_query(text);
   }
 
   function returnMessages(stateMessages) {
-    if (stateMessages.messages) {
-
+    // console.log(stateMessages)
+    if (stateMessages.messages ) {
+    
       return Object.entries(stateMessages.messages).map((message, i) => {
+        // console.log('messages', messages)
+        return messagesQuickReplies(message, i)
        
-        return <Message key={i} speaks={message[1]?.speaks} text={message[1]?.msg?.text?.text} />
       })
     } else {
       return null
--- front/src/components/chatbot/QuickReplies.js
+++ front/src/components/chatbot/QuickReplies.js
@@ -1,20 +1,44 @@
 import React from "react";
 import QuickReply from "./QuickReply";
+import img from './imgs/íris.png'
 
-
-const QuickReplies = () =>{
+const QuickReplies = (props) => {
 
 
   function handleClick(e, payload, text) {
-    replyClick(e, payload, text);
+    props.replyClick(e, payload, text);
+  }
+
+  function renderQuickReply (reply, i) {
+    return <QuickReply key={i} click={handleClick} reply={reply}/>
   }
 
 
+  function renderQuickReplies (quickReplies) {
+    if (quickReplies) {
+      return quickReplies.map((reply, i) => {
+        return renderQuickReplies(reply, i)
+      })
+    } else {
+      return null;
+    }
+  }
+
   return (
-    <div>
-      <div>
-        <div>
-          
+    <div className="wrapper">
+      <div className="panel">
+        <div className="panel-wrapper">
+          <div className=''>
+            <img src={img} alt='perfil' className='iris-perfil'/>
+          </div> 
+          <div>
+            {props.text && 
+              <p>
+                {props.text.stringValue}
+              </p>
+            }
+            {renderQuickReplies(props.payload)}
+          </div>
         </div>
       </div>
     </div>
